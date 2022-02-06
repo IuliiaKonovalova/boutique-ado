5618e9c423dd1d3f9fe1380a8aba5e707fb7efd5
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-02-06 23:13:58 +0800

    Change data in categories.json

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Category model """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ Return string representation """
+        return self.name
+
+    def get_friendly_name(self):
+        """ Return friendly name """
+        return self.friendly_name
