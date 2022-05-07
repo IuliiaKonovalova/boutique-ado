607899d79f9f769738440c01eb3ebe952fa4bd8e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-07 14:05:14 +0800

    import bag data to checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
 
 def checkout(request):
@@ -9,10 +10,14 @@ def checkout(request):
     if not bag:
         messages.error(request, "Your bag is empty")
         return redirect(reverse('products'))
-    
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     templte = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'client_secret': stripe_checkout_session['client_secret'],
     }
     return render(request, templte, context)
