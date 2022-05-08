283b3450561d8ba632ff74011157d6987e876286
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-08 14:40:27 +0800

    Add error item out of stock functionality

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.config import settings
 
 from .forms import OrderForm
+from .models import OrderLineItem
+from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
@@ -12,18 +14,66 @@ def checkout(request):
     # stripe public and secret keys
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "Your bag is empty")
-        return redirect(reverse('products'))
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_method(*args, **kwargs)
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency= settings.STRIPE_CURRENCY,
-    )
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please contact us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please check your information and try again.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "Your bag is empty")
+            return redirect(reverse('products'))
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_method(*args, **kwargs)
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency= settings.STRIPE_CURRENCY,
+        )
     order_form = OrderForm()
     # if the is no public key, return an error
     if not stripe_public_key:
