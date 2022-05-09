e7b44dac00c41a9fb07088df6a4a9a8f47b345ea
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-09 15:31:37 +0800

    Format checkout urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,12 @@
+"""Urls for checkout app."""
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('checkout_success/<int:order_id>/', views.checkout_success, name='checkout_success'),
+    path(
+        'checkout_success/<int:order_id>/',
+        views.checkout_success, name='checkout_success'
+    ),
 ]
